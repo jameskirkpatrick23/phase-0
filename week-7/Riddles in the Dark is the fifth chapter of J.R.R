08364a408a05b626ff70423352d964a09ae34856
James Kirkpatrick <jkirkpatrick1990@gmail.com>
2015-10-22 18:00:35 -0400

    initial working copy of 7.7

---
+++ week-7/Riddles in the Dark is the fifth chapter of J.R.R
@@ -0,0 +1,88 @@
+Riddles in the Dark is the fifth chapter of J.R.R Tolkien's The Hobbit. This chapter of The Hobbit holds the most importance to The Lord of the Rings as it deals with Bilbo's finding of the One Ring and the introduction of Gollum. This chapter has been heavily edited by Tolkien to bring it closer in line with the plot of The Lord of the Rings.
+Contents[show]
+PlotEdit
+Bilbo recovers from his fall and finds himself in a dark passage, finds a gold ring on the ground, and pockets it. Soon he comes across the creature Gollum. The two soon get engaged in a riddle war.
+SynopsisEdit
+Bilbo regains consciousness and, finding himself alone, tries to crawl through the tunnel. He stumbles across a ring, which he absentmindedly pockets. As a hobbit, Bilbo orients himself quite well underground, and he descends the tunnel until he comes to a lake.
+Bilbo is spotted by Gollum, a strange creature who lives alone on an island in the middle of the lake, which he navigates in a small boat. Gollum is a small, slimy, dark creature with pale eyes who eats Goblins and fish, named for the sound he makes when he swallows. Because he lives alone, Gollum is in the habit of speaking to himself in a hissing baby-talk, calling himself "my precious." Trying to assess Bilbo, he engages him in a game of riddles, at which Bilbo proves his equal.
+
+Gollum decides to get the better of Bilbo by using his "birthday present", the ring. When he wears it, he is invisible and can more easily snag his prey. He goes to the hiding place where he keeps the ring, but it's missing. Gollum correctly surmises that Bilbo has it and confronts him, asking what is in his pocket. Bilbo, who does not understand the power of the ring, slips it on his finger as Gollum runs to attack him. Bilbo becomes invisible, and Gollum cannot find him to attack.
+
+Gollum is afraid that without the invisibility of the ring, he will be captured by the Goblins, so he panics and runs to escape the cave. Bilbo, who is still invisible, follows him, thereby discovering the way out. Gollum, who can smell Bilbo, blocks his way. Bilbo thinks of killing Gollum, but decides it would not be fair, since Gollum is defenseless. He escapes by leaping over Gollum's head and through the passageway, but he runs into the Goblins. As they rush at him, he slips the ring on and escapes to the outside where it is light and the Goblins cannot follow.
+
+Changes from the first editionEdit
+The Lord of the Rings brought about changes to the context of the original story, and led to substantial changes to the character of Gollum.
+In the first edition of The Hobbit, Gollum bets his ring on the riddle game and if Bilbo wins, the ring will be presented as a present. After losing, Gollum seeks for the ring, but cannot find it. He begs for Bilbo's pardon and shows him the way out.
+
+Tolkien made Gollum more aggressive in the second edition to reflect on the concept of the ring's corrupting abilities.
+
+In The Lord of the Rings, the original version of the riddle game is explained as a "lie" made up by Bilbo under the harmful influence of the Ring, whereas the revised version contains the "true" account.
+
+RiddlesEdit
+These are the riddles that are told by Bilbo and Gollum in this chapter:
+Gollum
+What has roots as nobody sees,
+Is taller than trees Up, up, up it goes,
+And yet never grows?
+Answer: Mountain
+
+Bilbo
+Thirty white horses on a red hill,
+First they champ,
+Then they stamp,
+Then they stand still.
+Answer: Teeth on Chestnuts
+
+Gollum
+Voiceless it cries,
+Wingless flutters,
+Toothless bites,
+Mouthless mutters.
+Answer: Wind
+
+Bilbo
+An eye in a blue face
+Saw an eye in a green face.
+'That eye is like to this eye'
+Said the first eye,
+'But in low place
+Not in high place.'
+Answer: Sun shining on daisies
+
+Gollum
+It cannot be seen, cannot be felt,
+Cannot be heard, cannot be smelt.
+It lies behind stars and under hills,
+And empty holes it fills.
+It comes first and follows after,
+Ends life, kills laughter.
+Answer: Dark
+
+Bilbo
+A box without hinges, key or lid,
+Yet golden treasure inside is hid.
+Answer: Egg
+
+Gollum
+Alive without breath,
+As cold as death;
+Never thirsty, ever drinking,
+All in mail never clinking
+Answer: Fish
+
+Bilbo
+No-legs lay on one-leg, two legs sat near on three legs, four legs got some.
+Answer: Fish on a little one-legged table, man at table sitting on a three-legged stool, the cat gets the bones
+
+Gollum
+This thing all things devours:
+Birds, beasts, trees, flowers;
+Gnaws iron, bites steel;
+Grinds hard stones to meal;
+Slays king, ruins town,
+And beats high mountain down.
+Answer: Time
+
+Bilbo
+What have I got in my pocket?
+Answer: The Ring
--- week-7/game.js
+++ week-7/game.js
@@ -3,58 +3,144 @@
 // This is a solo challenge
 
 // Your mission description:
-//    The Lord of the Rings: Gollum's Cave
-//  You are an adventure seeking hobbit named Bilbo Baggins, and find yourself inside of a dark and mysterious cave, after narrowly avoiding a band of bloodthirsty goblins. Inside the cave, you find a magic ring. Fascinated, you barely notice as a creature sneaks up beside you and tries to smack you with a rock! How rude! In order to distract this eccentric underworlder, you propose a game of riddles. If you win the game of riddles, you are free to go, otherwise "we eats the hobbitses".
-// Overall mission: Win the game of riddles against gollum, and flee the cave
-// Goals: Complete three successful riddle questions and answers (prompts)
-// Characters: Gollum and Frodo
-// Objects: Riddle (Question, and Answer), Gollum, and Frodo
-// Functions: Ask Riddle, Check Answer
+// Overall mission:
+// Goals:
+// Characters:
+// Objects:
+// Functions:
 
 // Pseudocode
-//Give some sort of back story as a console.log
-//Offer three choices
-// Flee (adventre ends as you are killed by goblins), Attack (gollum kills you), and Offer to play a game (this begins the bulk of the program)
-//Display the rules for the game (Frodo asks, then Gollum asks)
-//Frodo asks first, give list of possible riddles and let user pick
-// Display gollum correct anwser
-// Gollum asks
-//Display possible answers to user. If incorrect guess, kill user
-// If correct, repeat with new riddle options.
-// Upon third riddle, give the user only one choice to ask gollum... What do I have in my pocket
-// Display gollums three guesses (with frodo responses)
-// Have gollum get mad and you run away from him to go join your comrades.
+//
+//
+//
+//
+//
 
 // Initial Code
+var riddleNumber = 0
+var stillAlive = true
+//DEFINE OUR CHARACTERS
+//GOLLUM
 var gollum = {
 
-  lines : ""
-  riddleQuestion: []
-  riddleAnswer: []
-}
-var smeagol = {
-  lines : ""
-  riddleQuestion: []
-  riddleAnswer: []
-}
-var bilbo = {
-  lines : ""
-  riddleQuestion: []
-  riddleOneAnswers:
-  riddleTwoAnswers:
-  riddleThreeAnswers:
-}
-// SET THE SCENE
-console.log("Blood pumping, tiny hobbit feet a blur of motion, Bilbo baggins blazed around the bend in the goblin-cave.")
-console.log("'There must be a place to hide...' Gazing about himself, Bilbo saw nothing inside the vast expanse of tunnels than rock. A red glow began to inch forward on the tunnel wall beside him. They were coming. The goblins had blood on their minds, and they were coming for Bilbo. 'Of all the times to have poor luck...' Frantically scanning his surroundings, Bilbo found what he was looking for. A crack in the wall. 'There!' Bilbo exclaimed as he threw himself inside the fissure.")
-console.log("")
-console.log("War cries and footsteps rang through the air, yet did not stop as they passed by Bilbo's hiding place. Sagging against the wall, Bilbo took a deep breath. He had escaped danger for now. With a sigh, Bilbo stood up, taking stock of his surroundings. On the ground to his left, he noticed a shimmering light. An unnatural glow that seemed to call to him. Begged him to investigate. And so, Bilbo had found the ring.")
-console.log("")
-console.log("It was at that moment, that Bilbo heard a rock crash against the ground to his right. Hastily tucking the ring into his pocket, Bilbo whirled around, unsheathing his sword Sting in the process")
-console.log("")
-console.log("")
-console.log("A raspy ")
+  lines: ['Is it lost precious?', 'SHUT UP!', 'Wasnt talking to you', 'And if it loses? What then precious?']
+};
+  gollum.riddleQuestion = ['Voiceless it cries, Wingless flutters, Toothless bites, Mouthless mutters']
+  gollum.riddleAnswer = ['Our turn', 'Sun shining on daisies precious', 'Eggs', 'What is it precious, what could it be']
 
+//SMEAGOL
+var smeagol = {
+    lines : ['Oh! We knows! We knows safe paths for hobbitses! Safe paths in the dark...', 'Well, if it loses, we eats it! If Baggins loses, we eats it whole!']
+}
+  smeagol.riddleQuestion = ['What is it?', 'It cannot be seen, cannot be felt, Cannot be heard, cannot be smelt. It lies behind stars and under hills, And empty holes it fills. It comes first and follows after, Ends life, kills laughter.', 'Alive without breath, As cold as death; Never thirsty, ever drinking, All in mail never clinking', 'This thing all things devours: Birds, beasts, trees, flowers; Gnaws iron, bites steel; Grinds hard stones to meal; Slays king, ruins town, And beats high mountain down.' ]
+  smeagol.riddleAnswer = ['Teeth? TEETH!!! HAHAH Yes my preciousss.', 'Daisies', 'Eggs precious... very good precious, very good... Now its our turn', 'Fishes on a little one-legged table, man at table sitting on a three-legged stool, the cat gets the boneses']
+//BILBO
+var bilbo = {
+    lines : ['Yes... Yes and I wish to get unlost... as soon as possible!', 'I didnt say anything...', 'Why dont we have a game of riddles. If I win, you show me out of here', 'Fair enough... Ill go first.']
+}
+  bilbo.riddleQuestion = ['Thirty white horses on a red hill, First they champ, Then they stamp, Then they stand still.', 'An eye in a blue face, Saw an eye in a green face. That eye is like to this eye, Said the first eye, But in low place, Not in high place.', 'A box without hinges, key or lid, Yet golden treasure inside is hid.', 'No-legs lay on one-leg, two legs sat near on three legs, four legs got some.', 'What have I got in my pocket?']
+
+var addSpaces = function (){
+  console.log('')
+  console.log('')
+}
+var callRiddle = function (riddleNumber){
+  switch (riddleNumber){
+    case 0:
+      console.log('Bilbo: ' + bilbo.riddleQuestion[0])
+      console.log('Smeagol: ' + smeagol.riddleAnswer[0])
+      console.log('Gollum: ' + gollum.riddleAnswer[0])
+      console.log('Gollum: ' + gollum.riddleQuestion[0])
+      console.log('Smeagol: ' + smeagol.riddleQuestion[0])
+
+
+      var answer = prompt("1. Winter 2. Wind 3. Love     4. Babies Enter the number of your guess here:");
+       if (answer != 2)
+         stillAlive = false
+       else
+         addSpaces()
+         console.log('Bilbo: Now its my turn again! ', 'Gollum: Very clever hobbitses... very clever...' );
+      break;
+    case 1:
+      console.log('Bilbo: ' + bilbo.riddleQuestion[1])
+      console.log('Smeagol: ' + smeagol.riddleAnswer[1] + 'Gollum: ' + gollum.riddleAnswer[1])
+      console.log('Smeagol: ' + smeagol.riddleQuestion[1])
+      var answer = prompt("1. Space      2. Time      3. Dark     4. Evil      Enter the number of your guess here:")
+      if (answer != 3)
+        stillAlive = false
+      else
+        console.log('Bilbo: My turn again!');
+        addSpaces()
+      break;
+    case 2:
+      console.log('Bilbo: ' + bilbo.riddleQuestion[2])
+      console.log('Gollum: ' + gollum.riddleAnswer[2])
+      console.log('Smeagol ' + smeagol.riddleAnswer[2])
+      console.log('Smeagol ' + smeagol.riddleQuestion[2])
+      var answer = prompt("1. Soldiers      2. Spiderwebs      3. Dwarves     4. Fish      Enter the number of your guess here:")
+      if (answer != 4)
+        stillAlive = false
+      else
+        addSpaces()
+        console.log('Gollum: Nasty hobbitses, must be cheating. Kills them prec-');
+      break;
+    case 3:
+      console.log('Bilbo: ' + bilbo.riddleQuestion[3])
+      console.log('Gollum: ' + gollum.riddleAnswer[3])
+      console.log('Bilbo: Give up yet?')
+      console.log('Smeagol: Give us a minute... Give us a chance preciousss')
+      console.log('Smeagol: ' + smeagol.riddleAnswer[3])
+      console.log('Smeagol: ' + smeagol.riddleQuestion[3])
+      var answer = prompt("1. Time      2. Death      3. Water     4. Stone      Enter the number of your guess here:")
+      if (answer != 1)
+        stillAlive = false
+      else
+        addSpaces()
+        console.log('Gollum: Lets just kill the hobbitses precious, I dont want to-');
+      break;
+    case 4:
+      console.log('Bilbo: ' + bilbo.riddleQuestion[4])
+      console.log('Gollum: Thats not fair... Its not a riddle precious...')
+      console.log('Smeagol: Fine... but we gets three guesses...')
+      console.log('Bilbo: Fair enough')
+      console.log('Gollum: Fishes')
+      console.log('Bilbo: Nope')
+      console.log('Smeagol: Handses!')
+      console.log('Bilbo: Wrong again!')
+      console.log('Gollum: Buttons')
+      console.log('Bilbo: Incorrect, now you must show me the way out')
+      console.log('Gollum: Nasty Hobbitses... what does it have in its pocketses...')
+      addSpaces()
+      console.log('As gollum thinks, he begins to get more and more upset. Knowing the situation could get dangerous at any moment, Bilbo takes his chance and sneaks away to safety.')
+      console.log('Congratulations, you have made it through Gollums cave!')
+      break;
+  }
+}
+  // SET THE SCENE
+  console.log('Blood pumping, tiny hobbit feet a blur of motion, Bilbo baggins blazed around the bend in the goblin-cave.')
+  console.log('There must be a place to hide... Gazing about himself, Bilbo saw nothing inside the vast expanse of tunnels than rock. A red glow began to inch forward on the tunnel wall beside him. They were coming. The goblins had blood on their minds, and they were coming for Bilbo. Of all the times to have poor luck... Frantically scanning his surroundings, Bilbo found what he was looking for. A crack in the wall. There! Bilbo exclaimed as he threw himself inside the fissure.')
+  addSpaces()
+  console.log('War cries and footsteps rang through the air, yet did not stop as they passed by Bilbos hiding place. Sagging against the wall, Bilbo took a deep breath. He had escaped danger for now. With a sigh, Bilbo stood up, taking stock of his surroundings. On the ground to his left, he noticed a shimmering light. An unnatural glow that seemed to call to him. Begged him to investigate. And so, Bilbo had found the ring.')
+  addSpaces()
+  console.log('It was at that moment, that Bilbo heard a rock crash against the ground to his right. Hastily tucking the ring into his pocket, Bilbo whirled around, unsheathing his sword Sting in the process')
+  addSpaces()
+  console.log('A raspy, almost hissing voice rang out in the darkness.' + gollum.lines[0])
+  console.log('Bilbo whirled about, seeking the source of the voice. Bilbo responded' + bilbo.lines[0])
+  console.log('Smeagol: ' + smeagol.lines[0])
+  console.log('Gollum: ' +  gollum.lines[1])
+  console.log('Bilbo: ' + bilbo.lines[1])
+  console.log('Gollum ' + gollum.lines[2])
+  console.log('A hunched creature slinked form the darkness. Large black eyes suited to the darkness keenly watched Bilbo as he re-positioned his sword to face the creature. How can I get out of this mess Bilbo thought to himself...')
+  addSpaces()
+  console.log('Bilbo: ' + bilbo.lines[2])
+  console.log('Gollum: ' + gollum.lines[3])
+  console.log('Smeagol: ' + smeagol.lines[1])
+  addSpaces()
+  console.log('Bilbo: ' + bilbo.lines[3])
+while (stillAlive && riddleNumber < 5){
+    callRiddle(riddleNumber)
+    riddleNumber += 1
+}
 
 // Refactored Code
 
