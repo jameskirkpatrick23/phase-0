8758f36f9e9e9c1e5876546518e4bbb956b4285f
James Kirkpatrick <jkirkpatrick1990@gmail.com>
2015-10-22 13:45:45 -0400

    initial steps solo challenge

---
+++ week-7/game.js
@@ -0,0 +1,74 @@
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+//    The Lord of the Rings: Gollum's Cave
+//  You are an adventure seeking hobbit named Bilbo Baggins, and find yourself inside of a dark and mysterious cave, after narrowly avoiding a band of bloodthirsty goblins. Inside the cave, you find a magic ring. Fascinated, you barely notice as a creature sneaks up beside you and tries to smack you with a rock! How rude! In order to distract this eccentric underworlder, you propose a game of riddles. If you win the game of riddles, you are free to go, otherwise "we eats the hobbitses".
+// Overall mission: Win the game of riddles against gollum, and flee the cave
+// Goals: Complete three successful riddle questions and answers (prompts)
+// Characters: Gollum and Frodo
+// Objects: Riddle (Question, and Answer), Gollum, and Frodo
+// Functions: Ask Riddle, Check Answer
+
+// Pseudocode
+//Give some sort of back story as a console.log
+//Offer three choices
+// Flee (adventre ends as you are killed by goblins), Attack (gollum kills you), and Offer to play a game (this begins the bulk of the program)
+//Display the rules for the game (Frodo asks, then Gollum asks)
+//Frodo asks first, give list of possible riddles and let user pick
+// Display gollum correct anwser
+// Gollum asks
+//Display possible answers to user. If incorrect guess, kill user
+// If correct, repeat with new riddle options.
+// Upon third riddle, give the user only one choice to ask gollum... What do I have in my pocket
+// Display gollums three guesses (with frodo responses)
+// Have gollum get mad and you run away from him to go join your comrades.
+
+// Initial Code
+var gollum = {
+
+  lines : ""
+  riddleQuestion: []
+  riddleAnswer: []
+}
+var smeagol = {
+  lines : ""
+  riddleQuestion: []
+  riddleAnswer: []
+}
+var bilbo = {
+  lines : ""
+  riddleQuestion: []
+  riddleOneAnswers:
+  riddleTwoAnswers:
+  riddleThreeAnswers:
+}
+// SET THE SCENE
+console.log("Blood pumping, tiny hobbit feet a blur of motion, Bilbo baggins blazed around the bend in the goblin-cave.")
+console.log("'There must be a place to hide...' Gazing about himself, Bilbo saw nothing inside the vast expanse of tunnels than rock. A red glow began to inch forward on the tunnel wall beside him. They were coming. The goblins had blood on their minds, and they were coming for Bilbo. 'Of all the times to have poor luck...' Frantically scanning his surroundings, Bilbo found what he was looking for. A crack in the wall. 'There!' Bilbo exclaimed as he threw himself inside the fissure.")
+console.log("")
+console.log("War cries and footsteps rang through the air, yet did not stop as they passed by Bilbo's hiding place. Sagging against the wall, Bilbo took a deep breath. He had escaped danger for now. With a sigh, Bilbo stood up, taking stock of his surroundings. On the ground to his left, he noticed a shimmering light. An unnatural glow that seemed to call to him. Begged him to investigate. And so, Bilbo had found the ring.")
+console.log("")
+console.log("It was at that moment, that Bilbo heard a rock crash against the ground to his right. Hastily tucking the ring into his pocket, Bilbo whirled around, unsheathing his sword Sting in the process")
+console.log("")
+console.log("")
+console.log("A raspy ")
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
