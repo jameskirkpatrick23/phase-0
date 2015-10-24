ec84c0848407903cd24382c11a84ab8e3d338b7e
James Kirkpatrick <jkirkpatrick1990@gmail.com>
2015-10-24 12:29:09 -0400

    7-8 group User Stories

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,18 @@
+// USER STORIES
+// As a user, I want to be able to sum parts, or the whole of an array
+
+// As a user I want to be able to sum an array that has an odd length
+
+// As a user I want to be able to sum an array that has an even length
+
+// As a user, I want to be able to get the average of all the elements in an array
+
+// As a user I want to be able to get the average of an array that has an odd length
+
+// As a user I want to be able to get the average of an array that has an even length
+
+// As a user, I want to be able to find the median of all the elements in an array
+
+// As a user I want to be able to find the median of an array that has an odd length
+
+// As a user I want to be able to find the median of an array that has an even length
